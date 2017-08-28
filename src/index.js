5fc63c27e8a6baf26e0b6922b24c2965135143b8
github username <github email address>
2017-08-28 14:17:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 // Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class Order extends React.Component {
+  render () {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+};
+
+Order.defaultProps = {
+  cone: true,
+  size: 'regular'
+};
+
+Order.propTypes = {
+  cone: propTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+      customerName: PropTypes.string.isRequired,
+      orderedAt: PropTypes.number.isRequired
+  }).isRequired
+};
